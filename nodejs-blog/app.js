4303142eb9eb1d14294cf5edbb15df5aea731ed0
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2025-04-13 13:40:01 +0400

    add admin middleware and add new post functionality

--- nodejs-blog/app.js
+++ nodejs-blog/app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 
@@ -12,6 +15,19 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "Alan Wake",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+  })
+);
 
 app.use(express.static("public"));
 
@@ -21,6 +37,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}.`);
